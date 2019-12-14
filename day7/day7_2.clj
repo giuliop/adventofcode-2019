c628f10e79b569d406f38bde4271517914636afe
giulio <giuliop@gmail.com>
2019-12-14 14:25:56 +0100

    Day 7 part 2

--- day7/day7_2.clj
+++ day7/day7_2.clj
@@ -1,6 +1,5 @@
 (require '[intcode-computer :as computer]
          '[clojure.set :as sets])
-;(import clojure.lang.PersistentQueue)
 
 (def input-signal 0)
 
@@ -14,32 +13,24 @@
   and return [[outputs] [memory] [inputs] program-counter ended?]"
   (computer/run input program pc false))
 
-;(defn load-inputs [inputs]
-  ;"Takes a sequence of inputs and returns a sequence of PersistentQueue
-  ;loaded with those inputs"
-  ;(let [inputs (map #(if (coll? %) % (vector %)) inputs)]
-    ;(map (fn [xs] (apply conj PersistentQueue/EMPTY xs)) inputs)))
-
 (defn try-settings [input-signal settings program]
   "Takes a sequence of settings for the amplifiers, an input signal, and
   a program and retun the output signal"
   (let [[s1 s2 s3 s4 s5] settings]
     (loop [memories (vec (repeat 5 program)) ; memory of the 5 amplifiers
-           ;inputs (vec (load-inputs [[input-signal s1] s2 s3 s4 s5]))
-           inputs [[input-signal s1] [s2] [s3] [s4] [s5]]
+           inputs [[s1 input-signal] [s2] [s3] [s4] [s5]]
            pcs [0 0 0 0 0] ; program counters of the 5 amplifiers
            amp 0] ; current ampliifer running its program (0-4)
       (let [[m i pc] (map #(% amp) [memories inputs pcs])
-            ;_ (println i m pc)
             [o m i pc ended?] (output-signal i m pc)]
-            (if (and ended? (= amp 5)) (last o)
-              (let [next-amp (mod (inc amp) 5)
-                    next-amp-input (conj (inputs next-amp) (last o))]
-                (recur (assoc memories amp m)
-                       (assoc (assoc inputs next-amp next-amp-input)
-                              amp i)
-                       (assoc pcs amp pc)
-                       next-amp)))))))
+        (if ended? (last i)
+          (let [next-amp (mod (inc amp) 5)
+                next-amp-input (conj (inputs next-amp) (last o))]
+            (recur (assoc memories amp m)
+                   (assoc (assoc inputs next-amp next-amp-input)
+                          amp i)
+                   (assoc pcs amp pc)
+                   next-amp)))))))
 
 (defn answer []
   "Return the quiz answer"
